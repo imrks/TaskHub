63d2ee6c3830396f576077db62381b62d3902b11
Rohan Shaw <m1053568@mindtree.com>
2020-06-03 00:20:49 +0530

    Changed Date Type

--- src/main/java/com/stackhack/taskmanagement/entity/Tasks.java
+++ src/main/java/com/stackhack/taskmanagement/entity/Tasks.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
-import java.sql.Date;
+import java.util.Date;
 
 @Entity
 @Table(name = "Task")

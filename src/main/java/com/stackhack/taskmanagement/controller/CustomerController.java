4749bbf616e4d4fc909fd5296d31d4d97a11553f
Rohan Shaw <m1053568@mindtree.com>
2020-06-03 13:14:57 +0530

    Commit

--- src/main/java/com/stackhack/taskmanagement/controller/CustomerController.java
+++ src/main/java/com/stackhack/taskmanagement/controller/CustomerController.java
@@ -1,5 +1,27 @@
 package com.stackhack.taskmanagement.controller;
 
-public class CustomerController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.stackhack.taskmanagement.entity.Customer;
+import com.stackhack.taskmanagement.repo.CustomerRepo;
+
+@CrossOrigin
+@RestController
+public class CustomerController {
+	@Autowired
+	CustomerRepo custRepo;
+	
+	@PostMapping(value="/signup")
+	public void signUp(@RequestBody Customer customer) {
+		try {
+			custRepo.save(customer);
+		}
+		catch (Exception e){
+			return;
+		}
+	}
 }
--- src/main/java/com/stackhack/taskmanagement/repo/CustomerRepo.java
+++ src/main/java/com/stackhack/taskmanagement/repo/CustomerRepo.java
@@ -1,5 +1,13 @@
 package com.stackhack.taskmanagement.repo;
 
-public interface CustomerRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.stackhack.taskmanagement.entity.Customer;
+
+@Repository
+@Transactional
+public interface CustomerRepo extends JpaRepository <Customer, Long> {
+	public Customer findCustomerByid(long id);
 }

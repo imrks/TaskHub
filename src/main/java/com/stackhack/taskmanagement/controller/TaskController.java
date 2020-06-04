e48612b26db7895ccd2db723cf1d9acb14ccf94c
Palash Choubey <M1053710@mindtree.com>
2020-06-04 21:05:47 +0530

    pulling new code

---
+++ src/main/java/com/stackhack/taskmanagement/controller/TaskController.java
@@ -0,0 +1,13 @@
+package com.stackhack.taskmanagement.controller;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@CrossOrigin(origins="*")
+@RestController
+public class TaskController {
+	
+}
